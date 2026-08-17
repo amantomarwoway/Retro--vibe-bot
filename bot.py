af63ec72852a186a637867b5c0885d351af89363
amantomarwoway <amantomar748@gmail.com>
2026-08-17 16:50:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ Retro Vibe Club - Purani Indian filmo ka khazana
 
 #PublicDomain #OldIndianMovie #NoCopyright #RetroVibeClub #4KMovie #IndianCine
 """
-tok = os.environ.get('FB_PAGE_TOKEN','').strip()
+tok = os.environ.get('FB_PAGE_ID','').strip()
 print(f"TOKEN LENGTH: {len(tok)}")
 if tok:
     with open('final_4k.mp4','rb') as vf:
